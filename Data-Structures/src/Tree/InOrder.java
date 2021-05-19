6bb45fe0ccbdfafe3cd6ddae4b250e4f3f6a1d92
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-05-19 11:32:37 -0400

    Inorder Traversal

---
+++ Data-Structures/src/Tree/InOrder.java
@@ -0,0 +1,13 @@
+package Tree;
+
+public class InOrder {
+
+	public static void inOrder(Node root) {
+
+		if (root == null) return;
+
+		inOrder(root.left);
+		System.out.print(root.data + " ");
+		inOrder(root.right);
+	}
+}
