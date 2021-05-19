ee27571f3f02b72755aa06a4fcd86e356d8fc5c3
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-05-19 12:13:04 -0400

    Tree Height

---
+++ Data-Structures/src/Tree/Height.java
@@ -0,0 +1,13 @@
+package Tree;
+
+public class Height {
+
+	public static int height(Node root) {
+
+		if (root == null) return -1;
+
+		int leftHeight = height(root.left);
+		int rightHeight = height(root.right);
+		return Math.max(leftHeight, rightHeight) + 1;
+	}
+}
