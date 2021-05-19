24d8f3b806f5ba569cd0455fb430afdc9db303ee
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-05-19 11:27:13 -0400

    Postorder Traversal

---
+++ Data-Structures/src/Tree/PostOrder.java
@@ -0,0 +1,14 @@
+package Tree;
+
+public class PostOrder {
+
+	static void postOrder(Node root) {
+
+		if (root == null) return;
+
+		postOrder(root.left);
+		postOrder(root.right);
+
+		System.out.print(root.data + " ");
+	}
+}
