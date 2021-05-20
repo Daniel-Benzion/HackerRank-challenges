0287f75270753415426378a4059f73c508529d41
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-05-20 18:16:15 -0400

    Insert into Binary Tree -added iterative solution

--- Data-Structures/src/Tree/Insert.java
+++ Data-Structures/src/Tree/Insert.java
@@ -11,4 +11,21 @@ public class Insert {
 
 		return root;
 	}
+
+	public static Node insertIteratively(Node root, int data) {
+		if (root == null) return new Node(data);
+
+		Node parent = null;
+		Node current = root;
+
+		while(current != null) {
+			parent = current;
+			if (data < current.data) {
+				current = current.left;
+			} else current = current.right;
+		}
+		if (data < parent.data) parent.left = new Node(data);
+		else parent.right = new Node(data);
+		return root;
+	}
 }
