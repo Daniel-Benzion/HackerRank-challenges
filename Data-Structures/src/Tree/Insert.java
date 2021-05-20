b564ebc4143a565db85549be68c17a000230f6c2
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-05-20 18:06:19 -0400

    Insert into Binary Tree

---
+++ Data-Structures/src/Tree/Insert.java
@@ -0,0 +1,14 @@
+package Tree;
+
+public class Insert {
+
+	public static Node insert(Node root, int data) {
+		if (root == null) return new Node(data);
+
+		if (data < root.data) root.left = insert(root.left, data);
+
+		if (data > root.data) root.right = insert(root.right, data);
+
+		return root;
+	}
+}
