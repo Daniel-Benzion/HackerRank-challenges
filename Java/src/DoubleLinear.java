6c7bb0ffff8398ac391c5befa9776ff00a9ebf7b
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-06-16 09:10:41 -0400

    Twice Linear

---
+++ Java/src/DoubleLinear.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class DoubleLinear {
+
+    public static int dblLinear(int n) {
+        if (n == 0) return 1;
+        SortedSet<Integer> set = new TreeSet<>();
+        set.add(1);
+        for (int i = 0; i < n; i++) {
+            int x = set.first();
+            set.add(x * 2 + 1);
+            set.add(x * 3 + 1);
+            set.remove(x);
+        }
+        return set.first();
+    }
+
+}
